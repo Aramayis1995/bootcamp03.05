b9ea431a6deb7c251896d1409cc14dfc2e68b83d
Aramayis <mayissargsyan1995@gmail.com>
2021-05-03 22:58:22 +0400

    ParseFloat

---
+++ parseFloat.js
@@ -0,0 +1,26 @@
+function ParseFloat(str) {
+  for (i = 0; i < str.length; i++) {
+    if (str[0] === " ") {
+      str = str.slice(1, str.length);
+    }
+  }
+  if (str.charCodeAt(0) < 48 || str.charCodeAt(0) > 57) {
+    console.log(NaN);
+    return;
+  }
+  j = 0;
+  for (let i = 0; i < str.length; i++) {
+    j += 1;
+    if (str[i] === ".") {
+      break;
+    }
+  }
+  let counter = str.slice(0, j);
+  for (j = j + 1; j < str.length; j++) {
+    if (str.charCodeAt(j) < 48 || str.charCodeAt(j) > 57) {
+      console.log(Number(str.slice(0, j)));
+      return;
+    }
+  }
+}
+ParseFloat("5.5.gk");
